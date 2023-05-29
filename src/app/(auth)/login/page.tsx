aed4a8d0cb7d01d999339124b63d0e0329ff6cb7
Zoran Simonovic <simonovic1980@gmail.com>
2023-05-29 14:28:17 +0200

    loading

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 import React from "react";
 
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 async function page() {
+  await wait(4000);
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/posts/1"
   );
